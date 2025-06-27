1a1f82d071cafdf21795a8f09c51ce7a78af8241
DogLoverPink <lowenalexa2@gmail.com>
2025-06-27 15:14:19 -0400

    remove (probably?) unneeded assets folder

--- data/sendactivation.ml
+++
@@ -1,6 +0,0 @@
-<SETUP borders=40>
-<BKGCOLOR #004770>
-<color 1><font medium>
-<center>A new activation e-mail has been sent!<BR><BR>
-<link cmd=main><custom 300,75 x:"OKAY"></link><BR><movecursor +0,+5>
-
